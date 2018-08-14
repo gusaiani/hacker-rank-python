d30d43a193d6fad2b0c5cbcc36d0e101843090f7
Gustavo Saiani <gustavo@poe.ma>
2018-08-14 16:07:23 -0300

    Compare the triplets with zip

--- test_compare_the_triplets.py
+++ test_compare_the_triplets.py
@@ -11,18 +11,26 @@ def solve(array1, array2):
         if array2[i] > array1[i]:
             array2_total = array2_total + 1
 
-    return (f"{array1_total} {array2_total}")
+    return f"{array1_total} {array2_total}"
+
+def solve2(array1, array2):
+    sum1 = sum(a > b for a, b in zip(array1, array2))
+    sum2 = sum(a < b for a, b in zip(array1, array2))
+    return f"{sum1} {sum2}"
 
 def test_simple_array_sum():
     array1 = [5, 6, 7]
     array2 = [3, 6, 10]
     assert solve(array1, array2) == '1 1'
+    assert solve2(array1, array2) == '1 1'
 
     array1 = [17, 28, 30]
     array2 = [99, 16, 8]
     assert solve(array1, array2) == '2 1'
+    assert solve2(array1, array2) == '2 1'
 
     array1 = [6, 8, 12]
     array2 = [7, 9, 15]
     assert solve(array1, array2) == '0 3'
+    assert solve2(array1, array2) == '0 3'
 
